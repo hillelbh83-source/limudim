24b91caa24cfd2671bd17c32ff2e479cb64e152b
Hillel <hillelben14@gmail.com>
2026-07-22 01:01:12 +0300

    Fix Compose compile imports

--- android/app/src/main/java/com/hillel/studyzone/ui/screens/Overlays.kt
+++ android/app/src/main/java/com/hillel/studyzone/ui/screens/Overlays.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -37,7 +38,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -556,6 +556,7 @@ fun ChatOverlay(
         {
             focusManager.clearFocus(force = true)
             keyboard?.hide()
+            Unit
         }
     }
     Box(modifier.imePadding().navigationBarsPadding()) {
